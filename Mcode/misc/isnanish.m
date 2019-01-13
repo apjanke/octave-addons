99f9d6a96c66454e499cb372ff5bda9975474e90
Andrew Janke <floss@apjanke.net>
2019-01-12 20:04:55 -0500

    Rename isna to isnanish for Octave compatibility

--- Mcode/misc/isnanish.m
+++ Mcode/misc/isnanish.m
@@ -1,18 +1,18 @@
-function out = isna(x)
-  %ISNA True for not-available values.
+function out = isnanish(x)
+  %ISNANISH True for NaN-like values.
   %
   % out = isna(x)
   %
-  % isna() is a unification of isnan(), isnat(), and ismissing(). This is 
+  % isnanish() is a unification of isnan(), isnat(), and ismissing(). This is 
   % provided so you can write generic polymorphic code that operates on data
   % types that provide any of those functions.
   %
   % For string inputs, it does ismissing(x).
-  % For datetime inputs, it does isnat(x).
+  % For datetime and duration inputs, it does isnat(x).
   % For all other inputs, it does isnan(x).
   %
   % This is an Octave extension.
-  if isa(x, 'datetime')
+  if isa(x, 'datetime') || isa(x, 'duration')
     out = isnat(x);
   elseif isa(x, 'string')
     out = ismissing(x);
